6a58a2e9e08fa025b1876fbed7bb16c11996b02d
ac-adekunle <lola.adekunle@gmail.com>
2015-09-02 13:00:30 -0500

    Add password confirmation to user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
   end
 
 end
