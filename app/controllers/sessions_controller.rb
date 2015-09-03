3361c2c588d10e1eb242200e4f091b7fc7bba1e7
ac-adekunle <lola.adekunle@gmail.com>
2015-09-02 21:19:40 -0500

    Update sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_username(params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by_username(params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to '/sessions/new'
+      render '/sessions/new'
     end
   end
 
