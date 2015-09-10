400f64e7edcc94cd3aa75367f9e5c2db3ecfd178
ac-adekunle <lola.adekunle@gmail.com>
2015-09-10 13:35:08 -0500

    Add list#show controller and view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,15 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
+  
+  def show
+  end
+
+
+  private
+
+  def find_list
+  	@list = List.find(params[:id])
+  end
 
 
 end
