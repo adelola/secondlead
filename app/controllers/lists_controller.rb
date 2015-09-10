fc52da0b6e32b34d4d49ae4fe4d09e7aabfc0428
ac-adekunle <lola.adekunle@gmail.com>
2015-09-10 17:46:36 -0500

    Add lists#new and lists#create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,38 @@ class ListsController < ApplicationController
   def show
   end
 
+  def new
+  	@list = List.new
+  end
+
+  def create
+  	@list = List.new(new_list_params)
+    @list.user = current_user
+  	if @list.save
+      redirect_to user_path(current_user), notice: "Added #{@list.name}"
+  	else
+  	  render :new
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 
   private
 
   def find_list
   	@list = List.find(params[:id])
+  end
+
+  def new_list_params
+    params.require(:list).permit(:name, :description)
   end
 
 
