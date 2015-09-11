e0f068d64f83cb52d56b4d79f12e581629cf25de
ac-adekunle <lola.adekunle@gmail.com>
2015-09-10 21:57:20 -0500

    Nest lists routes under users

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'example' => 'example#index'
   mount Buttercms::Engine => '/blog'
-  resources :lists, :only => [:show, :new, :create, :edit, :update, :destroy]
+  
   resources :genres
   resources :casts
   resources :sessions, :only => [:new, :create]
   delete '/sessions' => 'sessions#destroy'
   resources :dramas, only: [:index, :show]
-  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :lists
+  end 
+
 end
 
