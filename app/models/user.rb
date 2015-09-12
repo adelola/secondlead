24e8f047756439d98de421918edeebabd3b9586f
ac-adekunle <lola.adekunle@gmail.com>
2015-09-12 14:31:04 -0500

    Generate 3 default list for each new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,21 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6 }
+ 
+  after_create :create_watch_list, :create_watching_list, :create_watched_list
+  
+  private
+
+  def create_watch_list
+  	List.create(name: 'Watch', description:'Queued Dramas', user_id: self.id)
+  end
+
+  def create_watching_list	
+  	List.create(name: 'Watching', description:'Dramas in Progress', user_id: self.id)
+  end
+
+  def create_watched_list
+  	List.create(name: 'Watched', description:'Dramas completed', user_id: self.id)
+  end
 
 end
