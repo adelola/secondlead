ad0266ad81d171b9053caa7aea40f395355fb29e
ac-adekunle <lola.adekunle@gmail.com>
2015-09-18 20:14:16 -0500

    Update controllers and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @lists = @user.lists
   end
+  
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
 
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,13 @@ Rails.application.routes.draw do
   post '/dramas/:drama_id/add' => 'dramas#add', as: 'add_to_list'
  
   resources :users, only: [:show, :new, :create, :edit, :update, :destroy] do
+    member do
+      get :following, :followers
+    end
     resources :lists
   end 
+
+  resources :relationships, :only => [:create, :destroy]
 
 end
 
