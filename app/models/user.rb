6d4a181bad970b2de361decbe923e5ffa2f6a0fb
ac-adekunle <lola.adekunle@gmail.com>
2015-09-18 19:52:20 -0500

    Add ar associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
 
   has_many :lists
 
+  has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+
+  has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, length: { maximum: 50 }, uniqueness: true
