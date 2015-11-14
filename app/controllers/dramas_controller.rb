ee70025464c439846d2cd026ea626c516559498a
huyhuynh408 <huyhuynh85@gmail.com>
2015-11-14 17:48:58 -0600

    added show

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -6,14 +6,18 @@ class DramasController < ApplicationController
 
   def add
     if current_user
-    @list = current_user.lists.find_by(name: "Watched")
+      @list = current_user.lists.find_by(name: "Watched")
     end
   	if @list
-	  @drama = Drama.find_by(id: params[:drama_id])
+	    @drama = Drama.find_by(id: params[:drama_id])
 	  	unless @list.dramas.find_by(id: @drama.id)
 	  	  @drama.add_to_list(@list)
 	  	end
   	end
   	redirect_to dramas_path
+  end
+
+  def show
+
   end
 end
