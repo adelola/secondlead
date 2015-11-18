08d47f0e6afe69879090d4850dfaac0d7131921c
ac-adekunle <lola.adekunle@gmail.com>
2015-11-17 22:57:09 -0600

    Add dragging example to dramasctrl

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,45 +1,13 @@
 var App = angular.module('drag-and-drop', ['ngDragDrop','templates', 'ui.bootstrap','ui.router']);
-App.controller('oneCtrl', function($scope, $q) {
-  $scope.list1 = {title: 'Drag and Drop with default confirmation'};
-  $scope.list2 = {};
-  $scope.beforeDrop = function() {
-    var deferred = $q.defer();
-    if (confirm('Are you sure???')) {
-      deferred.resolve();
-    } else {
-      deferred.reject();
-    }
-    return deferred.promise;
-  };
-});
-App.controller('twoCtrl', function($scope, $q, $modal) {
-  $scope.list1 = {title: 'Drag and Drop with custom confirmation'};
-  $scope.list2 = {};
-  $scope.beforeDrop = function() {
-    var modalInstance = $modal.open({
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl'
-    });
-
-    return modalInstance.result;
-  };
-}).controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
-  $scope.ok = function () {
-    $modalInstance.close();
-  };
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-});
 
 App.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state('dramatest', {
-      url:'/dramatest',
+    .state('dramas-model', {
+      url:'/dramas',
       templateUrl: 'dramas-index.html',
-      controller:'testCtrl',
-      controllerAs: 'test'
+      controller:'DramasCtrl',
+      controllerAs: 'dramas'
     });
 
   $urlRouterProvider.otherwise('/');
--- app/assets/javascripts/angular/dramas/dramas-controller.js
+++ app/assets/javascripts/angular/dramas/dramas-controller.js
@@ -1,6 +1,14 @@
-
-
-App.controller('testCtrl', function(){
-   var dramas = this;
-   dramas.test = 'WORKING!';  
+App.controller('DramasCtrl', function($q) {
+  var dramas = this;
+  dramas.list1 = 'Drag and Drop with default confirmation';
+  dramas.list2 = {};
+  dramas.beforeDrop = function() {
+    var deferred = $q.defer();
+    if (confirm('Are you sure???')) {
+      deferred.resolve();
+    } else {
+      deferred.reject();
+    }
+    return deferred.promise;
+  };
 });
