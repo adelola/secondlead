50228614261b8bb3bc4e82b16f5e2b1b2eb9909e
huyhuynh408 <huyhuynh85@gmail.com>
2015-11-22 21:43:35 -0600

    updated seartch controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   def search
     if params[:q].present?
+      @results = Drama.search(params[:q], index_name: [Drama.searchkick_index.name, User.searchkick_index.name, Genre.searchkick_index.name, List.searchkick_index.name, Cast.searchkick_index.name])
+    else
+      @results = []
     end
   end
 end
