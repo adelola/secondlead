67762b5f07b166862197be8e493430c0e37cef36
huyhuynh408 <huyhuynh85@gmail.com>
2015-11-22 19:33:22 -0600

    updated more setup

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,6 @@
 class SearchController < ApplicationController
+  def search
+    if params[:q].nil?
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   mount Buttercms::Engine => '/blog'
-  
+
   resources :genres
   resources :casts
-  
+
   resources :sessions, :only => [:new, :create]
   delete '/sessions' => 'sessions#destroy'
-  
+
   resources :dramas, only: [:index, :show]
   post '/dramas/:drama_id/add' => 'dramas#add', as: 'add_to_list'
- 
+
   resources :users, only: [:show, :new, :create, :edit, :update, :destroy] do
     member do
       get :following, :followers
     end
     resources :lists
-  end 
+  end
 
   resources :relationships, :only => [:create, :destroy]
 
   resources :reviews, only: [:create, :update, :destroy]
 
-
+  get 'search', to: 'search#search'
 end
 
