39114380ae87e83e917de3b64b42b4666700c45f
huyhuynh408 <huyhuynh85@gmail.com>
2015-11-25 21:05:36 -0600

    able to get cast index up

--- app/assets/javascripts/angular/casts/casts-controller.js
+++ app/assets/javascripts/angular/casts/casts-controller.js
@@ -1,25 +1,6 @@
-// App.factory('Casts', ['$resource',function($resource){
-//  return $resource('/casts.json', {},{
-//  query: { method: 'GET', isArray: true },
-//  create: { method: 'POST' }
-//  })
-// }]);
-
-// App.controller("castsCtrl", ['$scope', '$resource', 'Casts', 'Cast', '$name' function($scope, $resource, Casts, Cast, $name) {
-//   $scope.casts = Casts.query();
-// }]);
-
-// App.controller('castsCtrl', ['$scope', function($scope) {
-//     $scope.casts = ['hi', 'world'];
-// }]);
-
-// App.factory('Casts', ['$resource',function($resource){
-//   return $resource('/casts.json', {},{
-//   query: { method: 'GET', isArray: true },
-//   create: { method: 'POST' }
-//   })
-// }]);
-
-App.controller("castsCtrl", ['$scope', '$resource', 'Casts', 'Cast', function($scope, $resource, Casts, Cast) {
-  $scope.casts = Casts.query(); //it's getting user collection
-}]);
+App.controller('castsCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('/casts.json').success(function(data) {
+      $scope.casts = data;
+    });
+  }]);
