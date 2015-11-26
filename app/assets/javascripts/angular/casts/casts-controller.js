474326dbdae3eff67bc9cb82c5f704a53fbb37ec
huyhuynh408 <huyhuynh85@gmail.com>
2015-11-25 20:36:34 -0600

    working on casts still

--- app/assets/javascripts/angular/casts/casts-controller.js
+++ app/assets/javascripts/angular/casts/casts-controller.js
@@ -1,15 +1,25 @@
-App.factory('Casts', ['$resource',function($resource){
- return $resource('/casts.json', {},{
- query: { method: 'GET', isArray: true },
- create: { method: 'POST' }
- })
-}]);
+// App.factory('Casts', ['$resource',function($resource){
+//  return $resource('/casts.json', {},{
+//  query: { method: 'GET', isArray: true },
+//  create: { method: 'POST' }
+//  })
+// }]);
 
-// App.controller('castsCtrl', function() {
-//   var cast = this;
-//    cast.name = 'Your mom!';
-// });
+// App.controller("castsCtrl", ['$scope', '$resource', 'Casts', 'Cast', '$name' function($scope, $resource, Casts, Cast, $name) {
+//   $scope.casts = Casts.query();
+// }]);
 
-App.controller("castsCtrl", ['$scope', '$resource', 'Casts', 'Cast', '$name' function($scope, $resource, Casts, Cast, $name) {
-  $scope.casts = Casts.query();
+// App.controller('castsCtrl', ['$scope', function($scope) {
+//     $scope.casts = ['hi', 'world'];
+// }]);
+
+// App.factory('Casts', ['$resource',function($resource){
+//   return $resource('/casts.json', {},{
+//   query: { method: 'GET', isArray: true },
+//   create: { method: 'POST' }
+//   })
+// }]);
+
+App.controller("castsCtrl", ['$scope', '$resource', 'Casts', 'Cast', function($scope, $resource, Casts, Cast) {
+  $scope.casts = Casts.query(); //it's getting user collection
 }]);
