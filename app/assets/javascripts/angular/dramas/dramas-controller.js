ee692dc4d7321e7440b681c13ae82fdb8ed97516
ac-adekunle <lola.adekunle@gmail.com>
2015-11-27 22:37:36 -0600

    Update dramactrl to fetch dramas from api using restangular

--- app/assets/javascripts/angular/dramas/dramas-controller.js
+++ app/assets/javascripts/angular/dramas/dramas-controller.js
@@ -1,22 +1,20 @@
-App.controller('DramasCtrl', [ '$http', function($http, $q) {
-  var dramas = this;
-  dramas.list1 = 'Drag and Drop with default confirmation';
-  
-  dramas.items = [];
-  $http.get('/dramas').then(function(response) {
-    dramas.items = response.data;
-    console.log(response.data);
-      }, function(errResponse) {
-    console.error('Error while fetching dramas')
-  });
+(function(){
+'use strict';
 
-  dramas.beforeDrop = function() {
-    var deferred = $q.defer();
-    if (confirm('Are you sure???')) {
-      deferred.resolve();
-    } else {
-      deferred.reject();
-    }
-    return deferred.promise;
-  };
-}]);
+angular
+  .module('secondLead')
+
+  .controller('DramasCtrl', [
+    'DramaModel', 
+    'Restangular', 
+    function(DramaModel, Restangular, $q) {
+    var dramas = this;
+    
+    dramas.items = DramaModel.getAll;
+   
+    dramas.setCurrentDrama = function(item){
+      dramas.currentDrama = item;
+    };
+  }])
+})();
+  
