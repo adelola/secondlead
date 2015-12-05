11bdb444fe45b2262e95b10b716f638a0aa28d79
ac-adekunle <lola.adekunle@gmail.com>
2015-12-05 01:00:35 -0600

    Add links

--- app/assets/javascripts/angular/lists/list-controller.js
+++ app/assets/javascripts/angular/lists/list-controller.js
@@ -6,11 +6,15 @@ angular
 
   .controller('ListCtrl', [
     'Gridster',
+    'user',
     'list',
-    function(Gridster,list) {
+    function(Gridster, user, list) {
     var ctrl = this;
     
     ctrl.items = list.dramas;
+    console.log(user.user);
+    ctrl.userID = user.user.id;
+
     
     ctrl.gridsterOpts = Gridster.getOptions();
  	  ctrl.pageSize = 20;
--- app/assets/javascripts/angular/lists/lists-controller.js
+++ app/assets/javascripts/angular/lists/lists-controller.js
@@ -10,7 +10,7 @@ angular
     function(Gridster,lists) {
     var ctrl = this;
     ctrl.items = lists;
-    
+    console.log(lists);
     ctrl.gridsterOpts = Gridster.getOptions();
   }])
 
