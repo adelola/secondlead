5f740c0776caac178ed51787c243fc79b9d5fd74
ac-adekunle <lola.adekunle@gmail.com>
2015-12-10 23:40:37 -0600

    Refactor auth token module

--- lib/tasks/auth_token.rb
+++ lib/tasks/auth_token.rb
@@ -2,16 +2,17 @@ require 'jwt'
 
 module AuthToken
   def self.issue_token(payload, exp=24.hours.from_now)
-    payload['end'] = exp.to_i 
+    payload['exp'] = exp.to_i 
     payload['aud'] = Rails.application.secrets.client_id
     JWT.encode(payload, Rails.application.secrets.client_secret, 'HS512')
-   end
+  end
 
-   def self.valid?(token)
-     begin 
-       JWT.decode(token, Rails.application.secrets.client_secret)
-     rescue
-       false
-     end 
-   end
+  def self.valid?(token)
+    begin 
+      JWT.decode(token, Rails.application.secrets.client_secret)
+    rescue
+      false
+    end 
+  end
+
 end
