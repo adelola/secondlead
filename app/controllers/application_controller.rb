fb27fb971a40a739f3670069410a484c88b8cd28
ac-adekunle <lola.adekunle@gmail.com>
2015-12-10 23:35:22 -0600

    Add authenticate helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,26 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :authenticate!
 
   def current_user
-	@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    @current_user ||= authenticate!
   end
 
   def logged_in?
     current_user != nil
+  end
+
+  def authenticate!
+    begin
+      token = request.headers['Authorization'].split(' ').last
+      payload, header = AuthToken.valid?(token)
+      @current_user = User.find_by(id: payload['user_id'])
+    rescue
+      render json: { error: 'Authorization header not valid'}, status: :unauthorized
+    end
   end
 
 end
