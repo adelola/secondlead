507ea8bc1df99c03da8b2de132646ea48971e190
ac-adekunle <lola.adekunle@gmail.com>
2015-12-11 23:58:41 -0800

    Add angular login controller

---
+++ app/assets/javascripts/angular/login/login-controller.js
@@ -0,0 +1,77 @@
+(function(){
+
+'use strict';
+
+angular
+  .module('secondLead')
+ 
+  .controller('LoginCtrl', [
+    'UserModel',
+    '$state', 
+    function (UserModel, $state) {
+    var login = this;
+
+    login.loading = false;
+
+    login.user = {
+      email: '',
+      password: '',
+      register: false
+    };
+
+    function register() {
+      UserModel.register({
+          email: login.user.email,
+          password: login.user.password
+      })
+      .then(onLogin)
+      .catch(onError)
+      .finally(onCompletion);
+    }
+
+    function onLogin() {
+      UserModel.login({
+          email: login.user.email,
+          password: login.user.password
+      })
+      .then(onSuccess)
+      .catch(onError)
+      .finally(onCompletion);
+    }
+
+    function onSuccess(result) {
+      $state.go('boards');
+    }
+
+    function onError(reason) {
+      login.error = reason.message;
+    }
+
+    function onCompletion() {
+      login.reset();
+    }
+
+    login.submit = function (user, isValid, isRegistering) {
+      if (isValid) {
+        login.loading = true;
+
+        if (isRegistering) {
+          register();
+        } else {
+          onLogin();
+        }
+      }
+    };
+
+    login.reset = function () {
+      login.loading = false;
+      login.user = {
+        email: '',
+        password: '',
+        register: false
+      };
+    };
+
+  }])
+
+})();
