6fe6b4e2bd5cec7dc9affd564005ea1fbab32a9f
ac-adekunle <lola.adekunle@gmail.com>
2015-12-11 23:57:52 -0800

    Refactor auth service

--- app/assets/javascripts/angular/common/services/auth-service.js
+++ app/assets/javascripts/angular/common/services/auth-service.js
@@ -1,8 +1,41 @@
 'use strict';
 
-angular.module('secondlead.common')
-  .factory('Auth', function ($firebaseAuth, ENDPOINT_URI) {
-    var ref = new Firebase(ENDPOINT_URI);
-    return $firebaseAuth(ref);
-  })
-;
+angular.module('secondLead.common')
+  .factory('Auth', function($http, LocalService, AccessLevels) {
+	return {
+	  authorize: function(access) {
+	    if (access === AccessLevels.user) {
+	      return this.isAuthenticated();
+	    } else {
+	      return true;
+	    }
+	  },
+
+	  isAuthenticated: function() {
+	    return LocalService.get('auth_token');
+	  },
+
+	  login: function(credentials) {
+	    var login = $http.post('/auth/authenticate', credentials);
+	    login.success(function(result) {
+	      LocalService.set('auth_token', JSON.stringify(result));
+	    });
+	    return login;
+	  },
+
+	  logout: function() {
+	    // The backend doesn't care about logouts, delete the token and you're good to go.
+	    LocalService.unset('auth_token');
+	  },
+
+	  register: function(formData) {
+	    LocalService.unset('auth_token');
+	    var register = $http.post('/auth/register', formData);
+	    register.success(function(result) {
+	      LocalService.set('auth_token', JSON.stringify(result));
+	    });
+	    return register;
+	  }
+
+	};
+  });
