bfed97190e976bd7c668e45f1a4324ff3bb7ed1c
ac-adekunle <lola.adekunle@gmail.com>
2015-12-16 23:50:32 -0600

    Add login funtion to angular user model

--- app/assets/javascripts/angular/common/models/user-model.js
+++ app/assets/javascripts/angular/common/models/user-model.js
@@ -5,14 +5,38 @@ angular
   .module('secondLead')
   
   .factory('UserModel',['Restangular', function(Restangular) {
-     var baseUsers = Restangular.all('users');
+    var baseUsers = Restangular.all('users');
+    var currentUser = null;
 
-     return {
+    return {
       getAll: baseUsers.getList().$object,  
+      
       getOne: function(userId) {
         return Restangular.one('users', userId).get()
+      },
+      
+      setCurrentUser:  function (user) {
+      currentUser = user;
+      },
+
+      login: function (user) {
+        return Auth.login({
+          email: user.username,
+          password: user.password
+        }, function(error, authData) {
+          if (error) {
+            currentUser = null;
+            console.error('Authentication failed:', error);
+          } else {
+            currentUser = authData.uid;
+            console.log('Logged in as:', authData.uid);
+          }
+        });
       }
-     };
+
+    };
+
   }])
+
 })();
   
