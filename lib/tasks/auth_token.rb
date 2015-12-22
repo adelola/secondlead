70b7df73a0528740f7932506222f5624bde2cbb6
ac-adekunle <lola.adekunle@gmail.com>
2015-12-22 17:51:28 -0600

    Add secret for jwt token

--- lib/tasks/auth_token.rb
+++ lib/tasks/auth_token.rb
@@ -3,13 +3,13 @@ require 'jwt'
 module AuthToken
   def self.issue_token(payload, exp=24.hours.from_now)
     payload['exp'] = exp.to_i 
-    payload['aud'] = Rails.application.secrets.client_id
-    JWT.encode(payload, Rails.application.secrets.client_secret, 'HS512')
+    # payload['aud'] = Rails.application.secrets.client_id
+    JWT.encode(payload, Rails.application.secrets.client_secret, 'HS256')
   end
 
   def self.valid?(token)
     begin 
-      JWT.decode(token, Rails.application.secrets.client_secret)
+      JWT.decode(token, Rails.application.secrets.client_secret, true, { :algorithm => 'HS256' })
     rescue
       false
     end 
