2bba044cd41a122c80a704c4bb57e358ee7a7f7c
ac-adekunle <lola.adekunle@gmail.com>
2016-01-05 22:48:38 -0600

    Add remove function to drama model

--- app/assets/javascripts/angular/common/models/drama-model.js
+++ app/assets/javascripts/angular/common/models/drama-model.js
@@ -11,6 +11,9 @@ angular
       getAll: baseDramas.getList().$object,
       getOne: function(dramaID) {
         return Restangular.one('dramas', dramaID).get()
+      },
+      delete: function(userID, listID, dramaID) {
+        return Restangular.one("users", userID).one("lists", listID).one("dramas", dramaID).remove()
       }
     };
   }])
