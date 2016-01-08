aef61dd388643b8c65e8482468abf25942552f4b
ac-adekunle <lola.adekunle@gmail.com>
2016-01-08 02:45:52 -0600

    Refactor create on server side dramas controller

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -2,23 +2,10 @@ class DramasController < ApplicationController
   respond_to :json, :html
 
   def index
-     @dramas = Drama.fetch
-     @genres = Genre.limit(20)
-     respond_with(@dramas)
+    @dramas = Drama.fetch
+    @genres = Genre.limit(20)
+    respond_with(@dramas)
   end
-
-  # def add
-  #   if current_user
-  #     @list = current_user.lists.find_by(name: "Watched")
-  #   end
-  # 	if @list
-	 #    @drama = Drama.find_by(id: params[:drama_id])
-	 #  	unless @list.dramas.find_by(id: @drama.id)
-	 #  	  @drama.add_to_list(@list)
-	 #  	end
-  # 	end
-  # 	redirect_to dramas_path
-  # end
 
   def show
     @drama = Drama.find_by(id: params[:id])
@@ -26,27 +13,25 @@ class DramasController < ApplicationController
   end
 
   def create
-    user = User.find_by(id: params[:user_id])
-    list = user.lists.find_by(id: params[:list_id])
-    drama = Drama.find_by(id: params[:drama_id])
+    list = List.find_by(id: params[:list_id])
+    drama = Drama.find_by(id: params[:id])
     unless list.dramas.find_by(id: drama.id)
       new_list_drama = drama.add_to_list(list)
-        if new_list_drama.save
-          render json: { message: "Drama successfully added to #{list.name}" }
-        else
-          render json: { errors: "Oops, something went wrong." }
-        end
+      if new_list_drama.save
+        render json: { message: "Drama successfully added to #{list.name}" }
+      else
+        render json: { errors: "Oops, something went wrong." }
+      end
     end
-    render json: { message: "Drama already in #{list.name}" } 
   end
 
   def destroy
     drama = ListDrama.find_by({drama_id: params[:id], list_id: params[:list_id]})
-      if drama.destroy
-        render json: { message: "Drama successfully deleted." }
-      else
-        render json: { errors: "Oops, something went wrong." }
-      end
+    if drama.destroy
+      render json: { message: "Drama successfully deleted." }
+    else
+      render json: { errors: "Oops, something went wrong." }
+    end
   end
 
 end
