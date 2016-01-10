e123e65fbcb2822411c0ff70041e08b6749d425b
ac-adekunle <lola.adekunle@gmail.com>
2016-01-09 22:44:47 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
 
   resources :genres
   resources :casts, defaults: {:format => 'json'}
-
-  resources :dramas, only: [:index, :show], defaults: {:format => 'json'}
+  
+  resources :dramas, only: [:index, :show], defaults: {:format => 'json'} do
+    resources :ratings, except: [:new, :edit]
+    resources :reviews, except: [:new, :edit]
+  end
+    
 
   resources :users,:defaults => {:format => "json"} do
     member do
@@ -18,13 +22,10 @@ Rails.application.routes.draw do
 
   resources :relationships, only: [:create, :destroy]
 
-  resources :reviews, only: [:create, :update, :destroy]
-
   match 'search', to: 'search#search', via: 'get'
   match '/auth/register',     to: 'auth#register',     via: 'post'
   match '/auth/login', to: 'auth#login', via: 'post'
   match '/auth/token_status', to: 'auth#token_status', via: 'get'
-  match '/dramas/:drama_id/add', to: 'dramas#add', as: 'add_to_list', via: 'post'
 
 end
 
