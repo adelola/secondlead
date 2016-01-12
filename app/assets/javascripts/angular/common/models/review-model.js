2413449e42d25178eb86489b5da6cd2f100b9d40
ac-adekunle <lola.adekunle@gmail.com>
2016-01-11 23:42:59 -0600

    Add find and delete functions to ang review model

--- app/assets/javascripts/angular/common/models/review-model.js
+++ app/assets/javascripts/angular/common/models/review-model.js
@@ -4,8 +4,12 @@
 angular
   .module('secondLead')
   
-  .factory('ReviewModel',['Restangular', 'store', function(Restangular, store) {
+  .factory('ReviewModel',['$http', 'Restangular', 'store', function($http, Restangular, store) {
     var currentUser = store.get('user');
+    function extract(result) {
+      return result.data;
+    };
+
     return {
       
       getAll: function(dramaID){
@@ -17,11 +21,19 @@ angular
       },
 
       create: function(dramaID, userID, review) {
-        return Restangular.one('dramas', dramaID).getList('reviews').post({drama_id: dramaID, body: review, reviewer_id: userID})
+        return Restangular.one('dramas', dramaID).all('reviews').post({drama_id: dramaID, body: review, reviewer_id: userID})
+      },
+
+      find: function(dramaID, userID){
+        return $http.get('/reviews/find', {params: {drama_id: dramaID, reviewer_id: userID}}).then(extract);
       },
 
       update: function(dramaID, reviewID,review){
         return Restangular.one('dramas', dramaID).one('reviews', reviewID).put({body: review})
+      },
+
+      delete: function(dramaID, reviewID){
+        return Restangular.one('dramas', dramaID).one('reviews', reviewID).remove()
       }
 
     };
