4cc1327a22eac49532c7a5cd4dda0e795f6cea19
ac-adekunle <lola.adekunle@gmail.com>
2016-01-11 19:13:32 -0600

    Create directive for replacing reviewer's id with username

---
+++ app/assets/javascripts/angular/users/name-user-directive.js
@@ -0,0 +1,21 @@
+(function(){
+'use strict';
+
+angular.module('secondLead')
+  .directive('nameUser', ['UserModel',function(UserModel) {
+    
+    return {
+	  restrict : 'A',
+	  template : '<span> {{userName}}</span>',
+	  scope : {
+	    userId : '=',
+	  },
+	  link: function(scope, elem, attrs){
+	  	UserModel.getOne(scope.userId).then(function(result){
+	  		scope.userName = (result.user.username);
+	  	})
+	  }
+	}
+  }])
+
+})();
