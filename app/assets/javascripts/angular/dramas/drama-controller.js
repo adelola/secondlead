3ec8d12595838f3c63a8e809f7faec634e44c928
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-13 17:43:39 -0600

    arranging and adding content

--- app/assets/javascripts/angular/dramas/drama-controller.js
+++ app/assets/javascripts/angular/dramas/drama-controller.js
@@ -15,15 +15,17 @@ angular
 
     ctrl.user = UserModel.currentUser();
     ctrl.name = drama.name;
+    ctrl.non_english_name = drama.non_english_name
     ctrl.episode_count = drama.episode_count
     ctrl.year = drama.release_date;
+    ctrl.description = drama.plot;
     ctrl.dramaId = drama.id;
 
     var avgRate = function(drama){
       RatingModel.getAvg(drama).then(function(result){
         ctrl.avgRating = result;
       });
-    } 
+    }
 
     avgRate(drama.id);
 
