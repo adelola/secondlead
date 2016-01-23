5c0870f41d33fabcbea659969886cd5a7f0aa77d
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-23 17:57:46 -0600

    styled avg rating

--- app/assets/javascripts/angular/dramas/drama-controller.js
+++ app/assets/javascripts/angular/dramas/drama-controller.js
@@ -15,8 +15,8 @@ angular
 
     ctrl.user = UserModel.currentUser();
     ctrl.name = drama.name;
-    ctrl.non_english_name = drama.non_english_name
-    ctrl.episode_count = drama.episode_count
+    ctrl.non_english_name = drama.non_english_name;
+    ctrl.episode_count = drama.episode_count;
     ctrl.year = drama.release_date;
     ctrl.description = drama.plot;
     ctrl.dramaId = drama.id;
