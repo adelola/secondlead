4be25375f7769709ab7457cf22b46ab9feb993c2
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-24 22:54:37 -0600

    changed select options

--- app/assets/javascripts/angular/dramas/dramas-controller.js
+++ app/assets/javascripts/angular/dramas/dramas-controller.js
@@ -20,27 +20,24 @@ angular
     ctrl.selectedList = {};
 
     ctrl.gridsterOpts = {
-        columns: 4, // the width of the grid, in columns
-        pushing: false, // whether to push other items out of the way on move or resize
-        floating: false, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
-        swapping: false, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
-        width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
-        colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
-        rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
-        margins: [5, 5], // the pixel distance between each widget
-        outerMargin: true, // whether margins apply to outer edges of the grid
-        isMobile: false, // stacks the grid items if true
-        mobileBreakPoint: 750, // if the screen is not wider that this, remove the grid layout and stack the items
-        mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
-        minColumns: 1, // the minimum columns the grid must have
-        minRows: 2, // the minimum height of the grid, in rows
+        columns: 4,
+        width: 'auto',
+        colWidth: 'auto',
+        rowHeight: 'match',
+        margins: [5, 5],
+        outerMargin: true,
+        isMobile: false,
+        mobileBreakPoint: 750,
+        mobileModeEnabled: true,
+        minColumns: 1,
+        minRows: 2,
         maxRows: 100,
-        defaultSizeX: 1, // the default width of a gridster item, if not specifed
-        defaultSizeY: 1, // the default height of a gridster item, if not specified
-        minSizeX: 1, // minimum column width of an item
-        maxSizeX: null, // maximum column width of an item
-        minSizeY: 1, // minumum row height of an item
-        maxSizeY: null, // maximum row height of an item
+        defaultSizeX: 1,
+        defaultSizeY: 1,
+        minSizeX: 1,
+        maxSizeX: null,
+        minSizeY: 1,
+        maxSizeY: null,
         resizable: {
            enabled: false
         },
