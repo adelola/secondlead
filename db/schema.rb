74c717f8e68574e15bfc41847e8664599da5c3b1
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-26 14:44:50 -0600

    changed seed to get newest first

--- db/schema.rb
+++ db/schema.rb
@@ -16,23 +16,6 @@ ActiveRecord::Schema.define(version: 20160126172438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
-    t.integer  "trackable_id"
-    t.string   "trackable_type"
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.string   "key"
-    t.text     "parameters"
-    t.integer  "recipient_id"
-    t.string   "recipient_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
-
   create_table "casts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",       null: false
@@ -45,23 +28,6 @@ ActiveRecord::Schema.define(version: 20160126172438) do
     t.integer  "age"
     t.string   "non_english_name"
   end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.string   "title"
-    t.text     "body"
-    t.string   "subject"
-    t.integer  "user_id",          null: false
-    t.integer  "parent_id"
-    t.integer  "lft"
-    t.integer  "rgt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "drama_casts", force: :cascade do |t|
     t.integer  "drama_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 
 # 244 pages on Viki
 (1..2).each do |num|
-  ScrapeVikiForDramaUrls.new("https://www.viki.com/explore?page=#{num}").urls.each do |url|
+  ScrapeVikiForDramaUrls.new("https://www.viki.com/explore?page=#{num}&sort=latest").urls.each do |url|
     ScrapeVikiDramaContent.new(url)
   end
 end
