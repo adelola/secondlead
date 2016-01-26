91ea5ff278bae3c94f15b1db2952dcfa39d272ca
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-26 11:45:46 -0600

    finished cast scrape

--- lib/tasks/scrape_cast_content.rb
+++ lib/tasks/scrape_cast_content.rb
@@ -14,7 +14,7 @@ class ScrapeCastContent
         raise e
       end
     end
-    binding.pry
+
     @drama = drama
     @dob = nil
     @blood_type = nil
@@ -34,20 +34,23 @@ class ScrapeCastContent
   def scrape_info
     @doc.search('.dl-horizontal > dt').map { |element| element.inner_text }.each_with_index do |header, index|
       contents = @doc.search('.dl-horizontal > dd').map { |element| element.inner_text }
-
-      if /born/i =~ header
+      if (/born/i =~ header) == 0
         @dob = Date.parse(contents[index].split("(").first)
         @age = age(@dob)
-      elsif /blood type/i =~ header
+      elsif (/blood type/i =~ header) == 0
         @blood_type = contents[index]
-      elsif /star sign/i =~ header
+      elsif (/star sign/i =~ header) == 0
         @star_sign = contents[index]
-      elsif /height/i =~ header
+      elsif (/height/i =~ header) == 0
         @height = contents[index]
-      elsif /weight/i =~ header
+      elsif (/weight/i =~ header) == 0
         @weight = contents[index]
       end
     end
+  end
+
+  def scrape_name
+    @doc.search('.display-2').map { |element| element.inner_text }[0].strip
   end
 
   def scrape_non_english_name
@@ -60,7 +63,16 @@ class ScrapeCastContent
 
   def add_content_to_db
     if @doc != false
-      cast = Cast.where(name: scrape_name, non_english_name: scrape_non_english_name).first_or_create
+      cast = Cast.where(
+        name: scrape_name,
+        non_english_name: scrape_non_english_name,
+        dob: @dob,
+        blood_type: @blood_type,
+        age: @age,
+        weight: @weight,
+        height: @height,
+        star_sign: @star_sign
+        ).first_or_create
       DramaCast.create(cast: cast, drama: @drama)
     end
   end
