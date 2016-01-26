8404a875d94b3805bd4b5727d4b617428f70d402
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-26 12:20:47 -0600

    changed seed to work for viki

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
-(4630..4730).each do |num|
-  ScrapeDramaContent.new("http://www.dramafever.com/drama/#{num}")
+# (4630..4730).each do |num|
+#   ScrapeDramaContent.new("http://www.dramafever.com/drama/#{num}")
+# end
+
+# 244 pages on Viki
+(1..2).each do |num|
+  ScrapeVikiForDramaUrls.new("https://www.viki.com/explore?page=#{num}").urls.each do |url|
+    ScrapeVikiDramaContent.new(url)
+  end
 end
