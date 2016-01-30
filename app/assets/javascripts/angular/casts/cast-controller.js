cb055585573458659fd05a393c66fb8635eff60f
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-29 21:56:59 -0600

    formatted code style

--- app/assets/javascripts/angular/casts/cast-controller.js
+++ app/assets/javascripts/angular/casts/cast-controller.js
@@ -1,22 +1,22 @@
 (function(){
-'use strict';
+  'use strict';
 
-angular
-  .module('secondLead')
+  angular
+    .module('secondLead')
 
-  .controller('CastCtrl', [
-    'cast',
-    function(cast) {
-    var ctrl = this;
+    .controller('CastCtrl', [
+      'cast',
+      function(cast) {
+      var ctrl = this;
 
-    ctrl.image_url = cast.image_url;
-    ctrl.picture_file_name = cast.picture_file_name;
-    ctrl.name = cast.name;
-    ctrl.dob = cast.dob;
-    ctrl.age = cast.age;
-    ctrl.height = cast.height;
-    ctrl.weight = cast.weight;
-    ctrl.star_sign = cast.star_sign;
-    ctrl.blood_type = cast.blood_type;
-  }])
+      ctrl.image_url         = cast.image_url;
+      ctrl.picture_file_name = cast.picture_file_name;
+      ctrl.name              = cast.name;
+      ctrl.dob               = cast.dob;
+      ctrl.age               = cast.age;
+      ctrl.height            = cast.height;
+      ctrl.weight            = cast.weight;
+      ctrl.star_sign         = cast.star_sign;
+      ctrl.blood_type        = cast.blood_type;
+    }]);
 })();
