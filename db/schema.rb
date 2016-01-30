f357bbb23e851bd0fc70de25fbac8bbf956b3fca
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-29 21:49:43 -0600

    added image url to cast

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126221415) do
+ActiveRecord::Schema.define(version: 20160130034845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160126221415) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.string   "image_url"
   end
 
   create_table "drama_casts", force: :cascade do |t|
