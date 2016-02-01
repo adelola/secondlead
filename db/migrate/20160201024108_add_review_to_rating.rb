9bf0e90854f228e1a869887a2cd38f46fa651c38
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-31 21:21:30 -0600

    added rating weight to review

--- db/migrate/20160201024108_add_review_to_rating.rb
+++ db/migrate/20160201024108_add_review_to_rating.rb
@@ -1,5 +1,6 @@
 class AddReviewToRating < ActiveRecord::Migration
   def change
     add_reference :reviews, :rating, index: true, foreign_key: true
+    add_column :reviews, :rating_weight, :string
   end
 end
