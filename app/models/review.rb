a6df997a8e75aa1d6b889c776fb702c98e566373
huyhuynh408 <huyhuynh85@gmail.com>
2016-01-31 20:40:46 -0600

    cleaned up spacing

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ActiveRecord::Base
   include ActsAsReviewable::Review
 
   belongs_to :drama
-  belongs_to :reviewer, class_name: "User" 
-
+  belongs_to :reviewer, class_name: "User"
 end
