2ae73ab9df0d55405512033e5895c6b17ee42ff0
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-03 22:59:25 -0600

    format styling

--- app/assets/javascripts/angular/dramas/drama-controller.js
+++ app/assets/javascripts/angular/dramas/drama-controller.js
@@ -1,49 +1,47 @@
 (function(){
 'use strict';
 
-angular
-  .module('secondLead')
+  angular
+    .module('secondLead')
 
-  .controller('DramaCtrl', [
-    'drama',
-    'Gridster',
-    'RatingModel',
-    'Restangular',
-    'UserModel',
-    'ListModel',
-    function (drama, Gridster, RatingModel, Restangular,UserModel, ListModel){
-    var ctrl = this;
+    .controller('DramaCtrl', [
+      'drama',
+      'Gridster',
+      'RatingModel',
+      'Restangular',
+      'UserModel',
+      'ListModel',
+      function (drama, Gridster, RatingModel, Restangular, UserModel, ListModel) {
+      var ctrl = this;
 
-    ctrl.drama = drama.drama;
-    ctrl.user = '';
-    ctrl.selectedList = {};
-    ctrl.name = drama.drama.name;
-    ctrl.non_english_name = drama.drama.non_english_name;
-    ctrl.episode_count = drama.drama.episode_count;
-    ctrl.year = drama.drama.release_date;
-    ctrl.description = drama.drama.plot;
-    ctrl.dramaId = drama.drama.id;
-    ctrl.image_url = drama.drama.image_url;
+      ctrl.drama            = drama.drama;
+      ctrl.user             = '';
+      ctrl.selectedList     = {};
+      ctrl.name             = drama.drama.name;
+      ctrl.non_english_name = drama.drama.non_english_name;
+      ctrl.episode_count    = drama.drama.episode_count;
+      ctrl.year             = drama.drama.release_date;
+      ctrl.description      = drama.drama.plot;
+      ctrl.dramaId          = drama.drama.id;
+      ctrl.image_url        = drama.drama.image_url;
 
-    var authorized = function () {
-      if (UserModel.currentUser()) {
-        ctrl.user = UserModel.currentUser();
-        ctrl.userLists = ListModel.currentUserLists(ctrl.user.id);
+      var authorized = function () {
+        if (UserModel.currentUser()) {
+          ctrl.user      = UserModel.currentUser();
+          ctrl.userLists = ListModel.currentUserLists(ctrl.user.id);
+        }
+      };
+
+      var avgRate = function (drama){
+        RatingModel.getAvg(drama).then(function(result){
+          ctrl.avgRating = result;
+        });
       }
-    };
 
-    var avgRate = function (drama){
-      RatingModel.getAvg(drama).then(function(result){
-        ctrl.avgRating = result;
-      });
-    }
-
-    var initialize = function () {
-      authorized();
-      avgRate(drama.drama.id);
-    };
-    initialize();
-
-    ctrl.gridsterOpts = Gridster.getOptions();
-  }])
+      var initialize = function () {
+        authorized();
+        avgRate(drama.drama.id);
+      };
+      initialize();
+    }])
 })();
