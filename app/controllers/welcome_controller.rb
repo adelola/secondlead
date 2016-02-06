bda7cd9808bfa7d6d33c2b93f425d8462d06b9e4
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-06 15:20:32 -0600

    made changes to updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @activities = PublicActivity::Activity.order('created_at DESC').limit(50)
+      .select do |activity|
+        !activity.key.include?("destroy")
+      end
   end
 end
