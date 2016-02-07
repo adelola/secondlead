ec012b6a2dbd8612914564167be2f2d1e128d4be
ac-adekunle <lola.adekunle@gmail.com>
2016-02-07 00:44:49 -0600

    Clean up auto search directive

--- app/assets/javascripts/angular/common/services/autosearch-directive.js
+++ app/assets/javascripts/angular/common/services/autosearch-directive.js
@@ -10,9 +10,9 @@
 	      query: "=",
 	      showSearch: "&"
 	    },
-        template: '<input id="search-input" ng-model="query" type="text" placeholder="Search" ng-keyup="keyPressed()" />',
-        link: function (scope, elem, attrs){
-          scope.lastSearchTerm = null;
+      template: '<input id="search-input" ng-model="query" type="text" placeholder="Search" ng-keyup="keyPressed()" />',
+      link: function (scope, elem, attrs){
+        scope.lastSearchTerm = null;
 	      scope.searchTimer = null;
 	      scope.pause = 500;
 	      scope.minLength = 3;
@@ -23,40 +23,39 @@
 	      };
 
 	      scope.processResults = function (users, casts, dramas){
-			scope.results = [];
-			SearchService.setUsers(users);
-			SearchService.setCasts(casts);
+			    SearchService.setUsers(users);
+			    SearchService.setCasts(casts);
 	        SearchService.setDramas(dramas);
 	      };
 
-          scope.startSearch = function(str) {
-            if (str.length >= scope.minLength) {
-              $http.get("/search", {params: {q: str}}).
-                success(function (responseData, status, headers, config){
-                  scope.processResults(responseData["users"],responseData["casts"], responseData["dramas"]);
-                  scope.showSearch();
-                }).
-                error(function (data, status, headers, config){
-                  console.log("error");
-                });
-                
+        scope.startSearch = function(str) {
+          if (str.length >= scope.minLength) {
+            $http.get("/search", {params: {q: str}}).
+              success(function (responseData, status, headers, config){
+                scope.processResults(responseData["users"],responseData["casts"], responseData["dramas"]);
+                scope.showSearch();
+              }).
+              error(function (data, status, headers, config){
+                console.log("error");
+              });
+              
+          }
+        };
+
+        scope.keyPressed = function (){
+          if (isNewSearchNeeded(scope.query, scope.lastSearchTerm)) {
+            scope.lastSearchTerm = scope.query
+            if (scope.searchTimer) {
+              $timeout.cancel(scope.searchTimer);
+            }
+            scope.searchTimer = $timeout(function() {
+              scope.startSearch(scope.query);
+              }, scope.pause);
+            } else {
+              event.preventDefault();
             }
           };
-
-          scope.keyPressed = function (){
-            if (isNewSearchNeeded(scope.query, scope.lastSearchTerm)) {
-              scope.lastSearchTerm = scope.query
-              if (scope.searchTimer) {
-                $timeout.cancel(scope.searchTimer);
-              }
-              scope.searchTimer = $timeout(function() {
-                scope.startSearch(scope.query);
-                }, scope.pause);
-              } else {
-                event.preventDefault();
-              }
-            };
-	      };
+        };
 
 	    };
 	}]);
