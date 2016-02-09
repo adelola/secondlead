35dec88fb570bd1eb5dc3835a6e2e3c8dc6af7a5
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-08 20:36:11 -0600

    added drama factory

--- spec/factories/dramas.rb
+++ spec/factories/dramas.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :drama do
-    
+    name "Korean Drama Name"
   end
-
 end
--- spec/models/drama_spec.rb
+++ spec/models/drama_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Drama, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  before(:each) do
+    DatabaseCleaner.start
+  end
+
+  after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  it "adds itself to list" do
+    @drama = FactoryGirl.build(:drama, name: "test")
+    @list = FactoryGirl.build(:list, name: "test")
+    @drama.add_to_list
+    expect(@list).to include(@drama)
+  end
 end
