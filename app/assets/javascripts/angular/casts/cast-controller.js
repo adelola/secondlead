b6d6590195ac2b5c5de12d89844ffa079ce35dc0
huyhuynh408 <huyhuynh85@gmail.com>
2016-02-12 18:10:05 -0600

    added dramas and gridster to controller

--- app/assets/javascripts/angular/casts/cast-controller.js
+++ app/assets/javascripts/angular/casts/cast-controller.js
@@ -5,19 +5,22 @@
     .module('secondLead')
 
     .controller('CastCtrl', [
-      'cast',
-      function(cast) {
+      'cast', 'Gridster',
+      function(cast, Gridster){
       var ctrl = this;
 
-      ctrl.image_url         = cast.image_url;
-      ctrl.picture_file_name = cast.picture_file_name;
-      ctrl.name              = cast.name;
-      ctrl.dob               = cast.dob;
-      ctrl.age               = cast.age;
-      ctrl.non_english_name  = cast.non_english_name;
-      ctrl.height            = cast.height;
-      ctrl.weight            = cast.weight;
-      ctrl.star_sign         = cast.star_sign;
-      ctrl.blood_type        = cast.blood_type;
+      ctrl.image_url         = cast.cast.image_url;
+      ctrl.picture_file_name = cast.cast.picture_file_name;
+      ctrl.name              = cast.cast.name;
+      ctrl.dob               = cast.cast.dob;
+      ctrl.age               = cast.cast.age;
+      ctrl.non_english_name  = cast.cast.non_english_name;
+      ctrl.height            = cast.cast.height;
+      ctrl.weight            = cast.cast.weight;
+      ctrl.star_sign         = cast.cast.star_sign;
+      ctrl.blood_type        = cast.cast.blood_type;
+      ctrl.dramas            = cast.dramas;
+
+      ctrl.gridsterOpts = Gridster.getOptions();
     }]);
 })();
